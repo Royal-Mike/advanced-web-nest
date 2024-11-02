760c67962960964930966060aa6d7dcf2f723224
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-11-02 23:33:16 +0700

    enable specific cors origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true });
+  app.enableCors({
+    origin: [
+      'http://localhost:5173//advanced-web-dev/user-registration/login',
+      'https://royal-mike.github.io/advanced-web-dev/user-registration/login'
+    ],
+    credentials: true
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
